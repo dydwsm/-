8b9e473a5882b83fe0a32943b235cea099ee556f
dydwsm <88753824+dydwsm@users.noreply.github.com>
2022-08-07 13:28:13 +0800

    Add files via upload

--- python_for_xtract_by_txt.py
+++ python_for_xtract_by_txt.py
@@ -4,6 +4,11 @@
 批量在指定截面输入荷载
 """
 
+import pandas as pd
+import os
+from datetime import datetime
+import shutil
+
 
 def section_loading(lines_list, section_name, bending_moment_direction, loading_name, loading_value):
     """
@@ -63,22 +68,20 @@ def batch_rename(file_path, new_ext):
     修改指定文件的后缀名
     :param file_path: 原文件路径
     :param new_ext: 新后缀名
-    :return: 原目录下新文件
+    :return: 原目录下新文件 和新文件路径
     """
     file_dir = os.path.dirname(file_path)
     file_name = os.path.basename(file_path)
 
-    list_file = os.listdir(file_dir)  # 返回指定目录
+    # list_file = os.listdir(file_dir)  # 返回指定目录
     ext = os.path.splitext(file_name)  # 返回文件名和后缀，列表形式
-    file_new_name = ext[0] + new_ext
+    str_time = datetime.now().strftime("%m%d_%H%M_%S")
+    file_name_new = ext[0] + str_time + new_ext
     os.rename(
         os.path.join(file_dir, file_name),
-        os.path.join(file_dir, file_new_name),
+        os.path.join(file_dir, file_name_new),
     )
-
-
-import pandas as pd
-import os
+    return file_name_new
 
 
 def main():
@@ -127,7 +130,12 @@ def main():
 
         for i in lines_list:
             f2.write(i)
-    batch_rename(file_path2, ".xpj")
+    dir_a_str = batch_rename(file_path2, ".xpj")
+    print(dir_a_str)
+
+    file_dir_xpj = os.path.dirname(file_path2) + "/" + dir_a_str
+    file_dir_xpj_new = os.path.dirname(excel_path) + "/" + dir_a_str
+    shutil.copy(file_dir_xpj, file_dir_xpj_new)  # 把xpj文件复制到Excel表格目录下
 
 
 if __name__ == '__main__':
